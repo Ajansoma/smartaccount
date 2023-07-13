fd31c57d198ac6e06e274feccab34a7011edacfb
Ajansoma <khadijahaabubakar@gmail.com>
2023-07-13 14:20:31 +0000

    initial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Chart from '../component/Home/Chart';
 import ProfitSalesRevenue from '../component/Home/ProfitSalesRevenue';
+import Reports from '../component/Home/Reports';
 
 const Home = () => {
   return (
     <div className="px-6 py-12">
       <ProfitSalesRevenue />
       <Chart />
+      <Reports />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary-100': '#5B5CA6',
+        'primary-100': '#918EDE',
         'primary-200': '#908CB8',
-        'primary-300': '#FAF8FF',
+        'primary-300': '#f4f4fc',
         'primary-400': '#918EDE',
+        'primary-500': '#efeff6',
         'sec-100': '#B162A9',
         'sec-200': '#f7eff6',
         'sec-300': '#F07096',
